d888ab7265793037487f87303bd172011a163032
nvonbulow <nick@nvonbulow.com>
2024-11-15 13:55:46 -0500

    Move camera matrix generation code to utility file

--- src/FractalCanvas/FractalUiLayer.tsx
+++ src/FractalCanvas/FractalUiLayer.tsx
@@ -1,7 +1,7 @@
 import React, { LC, useMemo } from "@use-gpu/live";
 import { Axis, Cartesian, Grid, Line, Plot, Point, Transform } from "@use-gpu/plot";
-import { mat3, mat4, vec2 } from "gl-matrix";
-import { IterationOptions, XForm } from "~/flame";
+import { mat4, vec2 } from "gl-matrix";
+import { getCameraMatrix, IterationOptions, XForm } from "~/flame";
 
 const TRIANGLE_COLORS = [
   "#ff0000",
@@ -69,42 +69,15 @@ export const FractalUiLayer: LC<FractalUiLayerProps> = ({
     height,
   } = iterationOptions;
 
-  const [range_x, range_y] = useMemo(() => {
-    const inv_zoom = 1 / camera_zoom;
-    const inv_aspect = height / width;
-
-    const range_x = vec2.fromValues(camera_x - inv_zoom, camera_x + inv_zoom);
-    const range_y = vec2.fromValues(camera_y - inv_zoom * inv_aspect, camera_y + inv_zoom * inv_aspect);
-
-    return [range_x, range_y];
-  }, [camera_x, camera_y, camera_zoom, width, height]);
-
   // compute the position of the origin in the camera space
   const origin = useMemo(() => {
-    // debugger;
-    const inv_zoom = 1 / camera_zoom;
-    const inv_aspect = height / width;
+    const cameraMat = getCameraMatrix({
+      viewportSize: [width, height],
+      camera: [camera_x, camera_y, camera_zoom],
+    });
 
-    const range_x = vec2.fromValues(camera_x - inv_zoom, camera_x + inv_zoom);
-    const range_y = vec2.fromValues(camera_y - inv_zoom * inv_aspect, camera_y + inv_zoom * inv_aspect);
-
-    console.log('ranges', range_x, range_y);
-
-    const x_factor = (width - 1) / (range_x[1] - range_x[0]);
-    const y_factor = (height - 1) / (range_y[0] - range_y[1]);
-
-    const scale_mat = mat3.fromValues(
-      x_factor, 0, 0,
-      0, y_factor, 0,
-      -x_factor * range_x[0], -y_factor * range_y[1], 1,
-    );
-
-    const origin = vec2.create();
-    vec2.transformMat3(origin, origin, scale_mat);
-
-    return origin;
+    return vec2.transformMat3(vec2.create(), origin, cameraMat);
   }, [ camera_x, camera_y, camera_zoom, width, height ]);
-
 
   return (
     <Plot>
--- src/flame/util.ts
+++ src/flame/util.ts
@@ -1,3 +1,4 @@
+import { mat3, vec2, vec3 } from "gl-matrix";
 import { XForm } from "./types";
 
 /**
@@ -10,5 +11,47 @@ export function normalizeXForms(xforms: XForm[]): XForm[] {
     ...xform,
     weight: xform.weight / totalWeight,
   }));
+}
+
+export interface GetCameraMatrixArgs {
+  // width of the viewport in pixels
+  viewportSize: vec2;
+  // position of the camera in world space
+  camera: vec3;
+};
+
+export function getRanges({
+  // size of the viewport in pixels
+  viewportSize: [width, height],
+  // position of the camera in world space
+  camera: [x, y, zoom],
+}: GetCameraMatrixArgs): [vec2, vec2] {
+  const inv_zoom = 1 / zoom;
+  const inv_aspect = height / width;
+
+  const range_x = vec2.fromValues(x - inv_zoom, x + inv_zoom);
+  const range_y = vec2.fromValues(y - inv_zoom * inv_aspect, y + inv_zoom * inv_aspect);
+
+  return [range_x, range_y];
+}
+
+export function getCameraMatrix({
+  // size of the viewport in pixels
+  viewportSize: [width, height],
+  // position of the camera in world space
+  camera: [x, y, zoom],
+}: GetCameraMatrixArgs): mat3 {
+  const [range_x, range_y] = getRanges({ viewportSize: [width, height], camera: [x, y, zoom] });
+
+  const x_factor = (width - 1) / (range_x[1] - range_x[0]);
+  const y_factor = (height - 1) / (range_y[0] - range_y[1]);
+
+  const scale_mat = mat3.fromValues(
+    x_factor, 0, 0,
+    0, y_factor, 0,
+    -x_factor * range_x[0], -y_factor * range_y[1], 1,
+  );
+
+  return scale_mat;
 }
 
